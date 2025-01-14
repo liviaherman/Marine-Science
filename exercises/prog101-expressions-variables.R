0ade7f8751c3c6fffa0048da765302e2bd896609
liviaherman <liviaherman@ucsb.edu>
2025-01-13 21:13:19 -0800

    Complete PROG101

--- exercises/prog101-expressions-variables.R
+++ exercises/prog101-expressions-variables.R
@@ -1,15 +1,19 @@
 # What do the following expressions do?
 temp_F <- temp_C * 9/5 - 32
+#Convert fahrenhiet to Celcius
 rect_area <- rect_length * rect_width
-
+#Finding the area of a rectangle
 # Fill in the question marks to complete these expressions
-????_area <- 1/2 * base * height
-cylinder_volume <- pi * ????^2 * ???
+rect_area <- 1/2 * base * height
+cylinder_volume <- pi * r^2 * h
 
   # What are the errors in these expressions?
-  speed_m_s <- distance_m * time_s
-acceleration m s2 <- speed_m_s / time_s
+speed_m_s <- distance_m * time_s
+#Speed is the distance divided by time
+acceleration_m s2 <- speed_m_s / time_s
+#Must add an underscore, for example acceleration_m_s2
 force_N < mass + acceleration_m_s2
+#should be force_N <- mass + acceleration_m_s2
 
 # The energy density of market squid is 4850 joules per gram[1]. If a 225 kg
 # Risso's dolphin needs 5 million joules of energy per day[2] and a typical
@@ -17,6 +21,14 @@ force_N < mass + acceleration_m_s2
 # eat each day? Solve the above word problem in R. Consider how many variables
 # you need to create, what to name them to make the code readable, and how to
 # combine them into expressions.
+energy_density <- 4850
+mass_squid <- 35
+energy_squid <- energy_density * mass_squid
+energy_squid <- 169750
+energy_dolphin <- 5000000
+number_squid <- energy_dolphin / energy_squi
+
+
 
 # [1] Price et al. 2024 https://doi.org/10.3389/fmars.2023.1345525
 # [2] Barlow et al. 2008 https://doi.org/10.3354/meps07695
