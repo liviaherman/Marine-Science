919c8d10ceaadb9a78301ca36a6a857f4fbf17fe
liviaherman <liviaherman@ucsb.edu>
2025-01-13 21:14:14 -0800

    Complete PROG101

--- exercises/prog101-multiple-vectors.R
+++ exercises/prog101-multiple-vectors.R
@@ -1,17 +1,26 @@
 # What does the following code do?
 # Tip: `%%` is the remainder operator. E.g. 10 %% 4 is 2.
 instrument_deployed_hm <- c(730, 915, 1345)
+#This code has values of time in terms of "hundreds of minutes"
 instrument_deployed_h <- floor(instrument_deployed_hm) / 100
+#This code gives you the full hour of each time by removing the decimal
 instrument_deployed_hdec <- (instrument_deployed_hm %% 100) / 60
+#This code uses the remainder operator to get the minutes and then uses %% to
+#divide the minutes by 60 to get the fraction of an hour.
 instrument_deployed <- instrument_deployed_h + instrument_deployed_hdec
+#This code adds the hours plus fractional hours to get the total time in hours
+#as a decimal
 
 # Based on the code above, calculate the duration of instrument deployments
 # using the instrument recovery times below. What units do the durations have?
 instrument_recovered_hm <- c(1600, 1920, 2015)
-???
-instrument_recovered <- ??? + ???
 
-instrument_durations <- ??? - ???
+instrument_recovered <- floor(instrument_recovered_hm/100)
+#Divide hhmm by 100 and take the floor
+instrument_durations <- (instrument_recovered_hm %% 100)/60
+#Use the remainder operator (%%) to get the minutes, then divide by 60 to
+# convert them to a fraction of an hour
 
 # Which site had the longest duration? Use conditional indexing.
 site <- c("Santa Cruz", "Santa Rosa", "San Miguel")
+Instrument_durations <- instrument_recovered - instrument_deployed
--- exercises/prog101-using-functions.R
+++ exercises/prog101-using-functions.R
@@ -1,13 +1,22 @@
 # Fill in the blanks so the answers are all 2. Use help for unfamiliar functions
 # if necessary.
-log10(????)
-????(4)
-ceiling(????.1)
+log10(100)
+log2(4)
+ceiling(1.1)
 
 # Find the functions for cosine and sine. What are the cosine and sine of 0?
+# The function for cosine and sine are cos() and sin()
+cos(0)
+sin(0)
+#The cos(0) is 1 while the sin(0) is 0
 
 # What do you think the following functions do? Just go off the name, don't look
 # them up.
 read.csv()
+# I think this function has something to do with analyzing a csv file which
+# stores data in a spreadsheet like format?
 boxplot()
+# I believe this function generates a box and whisper plot?
 startsWith()
+# I think this function tells the computer which data points to start with when
+# dealing with a large set of data??
